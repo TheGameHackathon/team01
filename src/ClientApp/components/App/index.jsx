e3c209cfbd64580288d9947cfedabe2f628a235e
CoderLiQ <coderliq@gmail.com>
2019-02-23 14:30:36 +0500

    moving implementd

--- src/ClientApp/components/App/index.jsx
+++ src/ClientApp/components/App/index.jsx
@@ -3,14 +3,14 @@ import styles from './styles.css';
 import Field from '../Field';
 
 export default class App extends React.Component {
-    constructor () {
+    constructor() {
         super();
         this.state = {
             score: 0,
             fieldState: {}
         };
     }
-    
+
     componentDidMount() {
         document.addEventListener("keydown", this.handleKeyPress, false);
 
@@ -25,36 +25,55 @@ export default class App extends React.Component {
     }
 
     handleKeyPress = (e) => {
-        switch (e.keyCode) {
-            case 37:
-                fetch(`/api/game/score`)
-                    .then(res => res.json())
-                    .then(res => this.onReponse(res) );
-        }
-        
+
+        this.move(e);
+
+    }
+
+    move = (e) => {
+
+        if (e.keyCode >= 37 && e.keyCode <= 40) {
+
+            let url = `/api/games/${this.keyCodes[e.keyCode]}`;
+
+            fetch(url)
+                .then(res => res.json())
+                .then(res => this.onReponse(res));
+
+            this.getScore();
+        };
+
+    }
+
+
+    getScore = () => {
+        fetch(`/api/game/score`)
+            .then(res => res.json())
+            .then(res => this.setState({ score: res }));
     }
 
     onReponse = (fieldState) => {
 
         console.log(fieldState);
-        this.setState({fieldState});
+        this.setState({ fieldState });
     }
 
     keyCodes = {
-        37: "left",
-        38: "up",
-        39: "right",
-        40: "down"
-    }
+        37: "moveLeft",
+        38: "moveUp",
+        39: "moveRight",
+        40: "moveDown"
+    };
 
-    render () {
+    render() {
         return (
             <div className={styles.root}>
-                <div className={ styles.score }>
-                    Ваш счет: { this.state.score }
+                <div className={styles.score}>
+                    Ваш счет: {this.state.score}
                 </div>
                 <Field fieldState={this.state.fieldState} />
             </div>
         );
     }
+
 }
