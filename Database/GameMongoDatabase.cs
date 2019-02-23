a2af87142a092c850b1dafde38ff8a98c240a9eb
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2019-02-23 11:05:44 +0500

    database creation added

---
+++ Database/GameMongoDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Database
+{
+    public static class GameMongoDatabase
+    {
+        public static IMongoDatabase Create()
+        {
+            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
+                                        ?? "mongodb://localhost:27017";
+            var mongoClient = new MongoClient(mongoConnectionString);
+            return mongoClient.GetDatabase("2048-games");
+        }
+    }
+}
