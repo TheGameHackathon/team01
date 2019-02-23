2f362f6202496096cb517d6418e4cf625bf17abe
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2019-02-23 11:22:47 +0500

    gameRepository interface added

---
+++ Database/IGameRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Database
+{
+    public interface IGameRepository
+    {
+        void Insert();
+
+        void Update(GameEntity game);
+
+        void UpdateOrInsert(GameEntity game);
+
+        void Delete(Guid gameId);
+    }
+}
---
+++ Database/Player.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Database
+{
+    public class Player
+    {
+        [BsonConstructor]
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        [BsonElement]
+        public string Name { get; }
+    }
+}
