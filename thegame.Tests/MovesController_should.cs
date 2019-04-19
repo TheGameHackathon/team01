94913222fc044d188433588af06063e512f72ac5
yel1ow <lv.reg.all@gmail.com>
2019-04-20 00:23:04 +0300

    added thegame.Tests

---
+++ thegame.Tests/MovesController_should.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using thegame.Controllers;
+using thegame.Models;
+
+namespace thegame.Tests
+{
+    public class MovesController_should
+    {
+        [Test]
+        public void NotFailWhenNullUserInput()
+        {
+            var userInput = new UserInputForMovesPost('c', null);
+            Assert.DoesNotThrow(() => new MovesController().Moves(Guid.NewGuid(), userInput));
+        }
+    }
+}
