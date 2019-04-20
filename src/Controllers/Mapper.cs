60cdd1916c8d295e704f985190e9ab03b4a60fb3
Vladimir <vladimir240298perm@gmail.com>
2019-04-20 13:03:09 +0300

    Create Mapper

--- src/Controllers/Mapper.cs
+++ src/Controllers/Mapper.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using thegame.Models;
 
 namespace thegame.Controllers
@@ -17,10 +19,26 @@ namespace thegame.Controllers
                 score:gameEntity.GetScore());
         }
 
-        private CellDto[] GetCells(GameEntity gameEntity)
+        private static CellDto[] GetCells(GameEntity gameEntity)
         {
-            gameEntity.GetTargets();
-            return null;
+            var id = 0;
+            var cells = new List<CellDto>();
+            ProcessObjects(cells, gameEntity.GetObjects(), 0, ref id);
+            ProcessObjects(cells, gameEntity.GetTargets(), 1, ref id);
+            return cells.ToArray();
         }
+
+        private static void ProcessObjects(ICollection<CellDto> cells, GameObject[,] objects, int zIndex, ref int id)
+        {
+            var width = objects.GetLength(1);
+            for (int i = 0; i < objects.GetLength(0); i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    cells.Add(new CellDto(id:id++.ToString(), pos: new Vec(j,i), type: objects[i,j].Type, content: "", zIndex: zIndex));
+                }
+            }
+        }
+
     }
 }
