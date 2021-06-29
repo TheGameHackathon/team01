eed90698e05b4d2971e6e7d721f87e2898db180f
Jaz7Mutant <d-a-ny4@yandex.ru>
2021-06-29 14:41:18 +0500

    Added AI

--- src/Services/Game.cs
+++ src/Services/Game.cs
@@ -28,6 +28,34 @@ namespace thegame.Services
             }
         }
 
+        public void MakeAIStep()
+        {
+            var initialColor = field.field[0, 0].Color;
+            var maxNeighbours = 0;
+            Color targetColor = Palette.colors[0];
+            var backup = new Color[field.Width, field.Height];
+            foreach (var cell in field.ConvertInOneLine())
+            {
+                backup[cell.Pos.X, cell.Pos.Y] = cell.Color;
+            }
+            foreach (var color in Palette.colors)
+            {
+                MakeStep(color, new Point(0,0));
+                var cellsToRepaint = GetConnectedArea(field.field[0,0]);
+                if (maxNeighbours < cellsToRepaint.Length)
+                {
+                    maxNeighbours = cellsToRepaint.Length;
+                    targetColor = color;
+                }
+
+                foreach (var cell in field.ConvertInOneLine())
+                {
+                    cell.Color = backup[cell.Pos.X, cell.Pos.Y];
+                }
+            }
+            MakeStep(targetColor, new Point(0,0));
+        }
+
         public bool Finished(Color color)
         {
             return field.ConvertInOneLine().All(c => c.Color == color);
