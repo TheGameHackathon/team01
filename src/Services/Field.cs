761af81a3f923c603c6ba3e6374e1165224ac695
Sergey M Solomkin <lynch513@yandex.ru>
2021-06-29 11:40:41 +0500

    Added Field implementation. Generate field

--- src/Services/Field.cs
+++ src/Services/Field.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 
 namespace thegame.Services
@@ -8,16 +9,33 @@ namespace thegame.Services
         public int Height { get; set; }
         public Cell[,] field;
 
+        private Random random;
+        public Palette Palette { get; set; }
+
         public Field(int width, int height, Palette palette)
         {
             Width = width;
             Height = height;
             field = new Cell[width, height];
+            random = new Random();
+            Palette = palette;
+
+            CreateField();
         }
 
         private void CreateField()
         {
-               
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    field[x, y] = new Cell(
+                        Guid.NewGuid().ToString(), 
+                        new Point(x, y), 
+                        Palette.colors[random.Next(0, 5)]
+                        );
+                }
+            }
         }
     }
 }
