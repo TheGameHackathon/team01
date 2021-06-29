0e2bf756320947591b46bec99e75b63a667a12fa
Sergey M Solomkin <lynch513@yandex.ru>
2021-06-29 11:25:38 +0500

    Added Palette implementation

--- src/Services/Palette.cs
+++ src/Services/Palette.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Drawing;
 
 namespace thegame.Services
 {
     public class Palette
     {
+        private Random random;
         public Color[] colors;
+
         public Palette(int colorCount)
         {
-            
+            random = new Random();
+            for(int i = 0; i < colorCount; i++)
+            {
+                colors[i] = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+            }
         }
     }
 }
